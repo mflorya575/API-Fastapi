76397f3c6f45b8e61f2cf0b829ac40c1bf6bff96
Michael Florya <mih2303@mail.ru>
2024-08-31 12:06:42 +0300

    create 2 models & post router

--- app/main.py
+++ app/main.py
@@ -1,17 +1,42 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from typing import Optional
 from datetime import date
+
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
+class HotelsSearchArgs:
+    def __init__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            has_spa: Optional[bool] = None,
+            stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
 @app.get('/hotels')
 def get_hotels(
-        location: str,
-        date_from: date,
-        date_to: date,
-        has_spa: Optional[bool] = None,
-        stars: Optional[int] = Query(None, ge=1, le=5),
+        search_args: HotelsSearchArgs = Depends()
 ):
-    return date_from, date_to
+    return search_args
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post('/bookings')
+def add_booking(booking: SBooking):
+    pass
