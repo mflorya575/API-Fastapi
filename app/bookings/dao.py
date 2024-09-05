dad5d1772830b8274d29256fa46dbef72cf6c5ac
Michael Florya <mih2303@mail.ru>
2024-09-05 12:21:35 +0300

    restructure dao

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,14 +1,6 @@
-from sqlalchemy import select
-from app.database import async_session_maker
-
 from app.bookings.models import Bookings
+from app.dao.base import BaseDAO
 
 
-class BookingDAO:
-
-    @classmethod
-    async def find_all(cls):
-        async with async_session_maker() as session:
-            query = select(Bookings)
-            bookings = await session.execute(query)
-            return bookings.scalars().all()
+class BookingDAO(BaseDAO):
+    model = Bookings
