3ecb4cbef68f95d4a49b38809ef92c4bc6d71b00
Michael Florya <mih2303@mail.ru>
2024-09-11 16:20:22 +0300

    create page hotels

--- app/main.py
+++ app/main.py
@@ -6,12 +6,17 @@ from pydantic import BaseModel
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
+from app.users.router import router as router_auth
+from app.pages.router import router as router_pages
 
 
 app = FastAPI()
 
+app.include_router(router_auth)
 app.include_router(router_users)
 app.include_router(router_bookings)
+
+app.include_router(router_pages)
 
 
 class HotelsSearchArgs:
